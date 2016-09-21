6b11a76ff4986865763d6131c8225c92ba83cedd
think2011 <452125301@qq.com>
2016-09-21 19:10:09 +0800

    temp scene2

--- clsses/Scene1.js
+++ clsses/Scene1.js
@@ -1,15 +1,34 @@
 class Scene1 {
     constructor() {
         this.$container     = document.querySelector('#scene1')
+        this.$nextContainer = document.querySelector('#scene2')
         this.$chatContainer = this.$container.querySelector('.chat-container')
         this.$prize         = this.$chatContainer.querySelectorAll('.item')[3]
         this.$sendContainer = this.$container.querySelector('.send-container')
+        this.$start         = this.$container.querySelector('.start')
 
+        this.init()
+    }
+
+    init() {
         this.$prize.addEventListener('animationend', () => {
             setTimeout(() => {
                 this.startTyping()
             }, 300)
         }, false)
+
+        // 开始游戏
+        this.$start.addEventListener('click', () => {
+            this.$container.addEventListener('animationend', () => {
+                this.$container.remove()
+            }, false)
+
+            this.$container.classList.add('bounceOutLeft', 'animated')
+            this.$nextContainer.style.display = 'block'
+            this.$nextContainer.classList.add('bounceInRight', 'animated')
+        }, false)
+
+        this.$container.style.display = 'block'
     }
 
     startTyping() {
@@ -23,7 +42,6 @@ class Scene1 {
             .addScene('typeing:😏 多大的事啊, 我来帮你吧!', 2500)
             .addScene(theater.replay.bind(theater))
 
-        this.$sendContainer.classList.add('bounceInUp')
-        this.$sendContainer.classList.add('active')
+        this.$sendContainer.classList.add('bounceInUp', 'active')
     }
 }
